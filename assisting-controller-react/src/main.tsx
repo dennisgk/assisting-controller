98f904f04c2957eb698dbf0eb75f37f900b9d270
Dennis Kountouris <dennisgk2005@gmail.com>
2024-10-24 23:15:17 -0400

    commit

--- assisting-controller-react/src/main.tsx
+++ assisting-controller-react/src/main.tsx
@@ -3,6 +3,7 @@ import "./stylesheets/index.css";
 import { utils, pages, deps } from "./meta";
 
 //https://cdn.jsdelivr.net/npm/monaco-editor@0.43.0/min/vs/loader.js
+//C:\Users\DennisK\Desktop\Folders\Projects\assisting-controller\assisting-controller-react\node_modules\monaco-editor\min
 deps.monaco.loader.config({
   paths: {
     vs: "/assets",
